f641e56d1e1ba5914ee0825240c166e76642915c
evertonsantos <125040234+everton-santoss@users.noreply.github.com>
2025-04-15 23:53:49 +0000

    Nivel mestre

--- batalhaNaval2.c
+++ batalhaNaval2.c
@@ -1,64 +1,135 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define TAM 10
-#define TAM_NAVIO 3
+#define TAM_HABILIDADE 5
 
-
-int podePosicionar(int tabuleiro[TAM][TAM], int linha, int coluna, int direcao, int diagonal) {
-    for (int i = 0; i < TAM_NAVIO; i++) {
-        int l = linha + (direcao == 1 ? i : 0) + (diagonal == 1 ? i : 0) - (diagonal == 2 ? i : 0);
-        int c = coluna + (direcao == 2 ? i : 0) + (diagonal == 1 ? i : 0) + (diagonal == 2 ? -i : 0);
-
-        if (l < 0 || l >= TAM || c < 0 || c >= TAM || tabuleiro[l][c] == 3) {
-            return 0; 
-        }
-    }
-    return 1;
-}
-
-void posicionarNavio(int tabuleiro[TAM][TAM], int linha, int coluna, int direcao, int diagonal) {
-    for (int i = 0; i < TAM_NAVIO; i++) {
-        int l = linha + (direcao == 1 ? i : 0) + (diagonal == 1 ? i : 0) - (diagonal == 2 ? i : 0);
-        int c = coluna + (direcao == 2 ? i : 0) + (diagonal == 1 ? i : 0) + (diagonal == 2 ? -i : 0);
-
-        tabuleiro[l][c] = 3;
-    }
-}
-
-int main() {
-    int tabuleiro[TAM][TAM] = {0}; 
-
-    int linha1 = 0, coluna1 = 0;
-    if (podePosicionar(tabuleiro, linha1, coluna1, 2, 0)) {
-        posicionarNavio(tabuleiro, linha1, coluna1, 2, 0);
-    }
-
-    int linha2 = 0, coluna2 = 5;
-    if (podePosicionar(tabuleiro, linha2, coluna2, 1, 0)) {
-        posicionarNavio(tabuleiro, linha2, coluna2, 1, 0);
-    }
-
-    int linha3 = 3, coluna3 = 3;
-    if (podePosicionar(tabuleiro, linha3, coluna3, 0, 1)) {
-        posicionarNavio(tabuleiro, linha3, coluna3, 0, 1);
-    }
-
-    int linha4 = 5, coluna4 = 7;
-    if (podePosicionar(tabuleiro, linha4, coluna4, 0, 2)) {
-        posicionarNavio(tabuleiro, linha4, coluna4, 0, 2);
-    }
-
-    printf("Tabuleiro de Batalha Naval (3 = navio, 0 = água):\n\n");
+void inicializarTabuleiro(int tabuleiro[TAM][TAM]) {
     for (int i = 0; i < TAM; i++) {
         for (int j = 0; j < TAM; j++) {
-            if (tabuleiro[i][j] == 3) {
-                printf("(3) ");
-            } else {
-                printf(" 0  ");
+            tabuleiro[i][j] = 0;
+        }
+    }
+
+    tabuleiro[2][2] = 3;
+    tabuleiro[3][3] = 3;
+    tabuleiro[4][4] = 3;
+    tabuleiro[5][5] = 3;
+    tabuleiro[6][6] = 3;
+}
+
+void imprimirTabuleiro(int tabuleiro[TAM][TAM]) {
+    printf("\nTabuleiro:\n");
+    for (int i = 0; i < TAM; i++) {
+        for (int j = 0; j < TAM; j++) {
+            switch (tabuleiro[i][j]) {
+                case 0: printf("0 "); break; 
+                case 3: printf("3 "); break; 
+                case 5: printf("5 "); break; 
+                default: printf("? "); break;
             }
         }
         printf("\n");
     }
+}
+
+
+
+void preencherCone(int matriz[TAM_HABILIDADE][TAM_HABILIDADE]) {
+    for (int i = 0; i < TAM_HABILIDADE; i++) {
+        int inicio = 2 - i;
+        int fim = 2 + i;
+        for (int j = 0; j < TAM_HABILIDADE; j++) {
+            if (j >= inicio && j <= fim) {
+                matriz[i][j] = 1;
+            } else {
+                matriz[i][j] = 0;
+            }
+        }
+    }
+}
+
+
+
+
+
+void preencherCruz(int matriz[TAM_HABILIDADE][TAM_HABILIDADE]) {
+    for (int i = 0; i < TAM_HABILIDADE; i++) {
+        for (int j = 0; j < TAM_HABILIDADE; j++) {
+            if (i == 2 || j == 2) {
+                matriz[i][j] = 1;
+            } else {
+                matriz[i][j] = 0;
+            }
+        }
+    }
+}
+
+
+
+void preencherOctaedro(int matriz[TAM_HABILIDADE][TAM_HABILIDADE]) {
+    int centro = 2;
+    for (int i = 0; i < TAM_HABILIDADE; i++) {
+        for (int j = 0; j < TAM_HABILIDADE; j++) {
+            if (abs(i - centro) + abs(j - centro) <= 2) {
+                matriz[i][j] = 1;
+            } else {
+                matriz[i][j] = 0;
+            }
+        }
+    }
+}
+void aplicarHabilidade(int tabuleiro[TAM][TAM], int habilidade[TAM_HABILIDADE][TAM_HABILIDADE], int origemX, int origemY) {
+    for (int i = 0; i < TAM_HABILIDADE; i++) {
+        for (int j = 0; j < TAM_HABILIDADE; j++) {
+            int x = origemX + i - 2;
+            int y = origemY + j - 2;
+
+            if (x >= 0 && x < TAM && y >= 0 && y < TAM) {
+                if (habilidade[i][j] == 1 && tabuleiro[x][y] != 3) {
+                    tabuleiro[x][y] = 5; // Marca como área de habilidade
+                }
+            }
+        }
+    }
+}
+
+
+void imprimirMatrizHabilidade(int matriz[TAM_HABILIDADE][TAM_HABILIDADE], const char* nome) {
+    printf("\nMatriz de habilidade: %s\n", nome);
+    for (int i = 0; i < TAM_HABILIDADE; i++) {
+        for (int j = 0; j < TAM_HABILIDADE; j++) {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int tabuleiro[TAM][TAM];
+    int cone[TAM_HABILIDADE][TAM_HABILIDADE];
+    int cruz[TAM_HABILIDADE][TAM_HABILIDADE];
+    int octaedro[TAM_HABILIDADE][TAM_HABILIDADE];
+
+    inicializarTabuleiro(tabuleiro);
+
+    preencherCone(cone);
+    preencherCruz(cruz);
+    preencherOctaedro(octaedro);
+
+    imprimirMatrizHabilidade(cone, "Cone");
+    aplicarHabilidade(tabuleiro, cone, 2, 5);
+    imprimirTabuleiro(tabuleiro);
+
+    preencherCruz(cruz); 
+    aplicarHabilidade(tabuleiro, cruz, 5, 5);
+    imprimirMatrizHabilidade(cruz, "Cruz");
+    imprimirTabuleiro(tabuleiro);
+
+    preencherOctaedro(octaedro);
+    aplicarHabilidade(tabuleiro, octaedro, 7, 3);
+    imprimirMatrizHabilidade(octaedro, "Octaedro");
+    imprimirTabuleiro(tabuleiro);
 
     return 0;
 }
